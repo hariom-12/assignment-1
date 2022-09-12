a01da549e8d21f3d10680a62e4edd5a8f1302bf6
Hari Om <hmishra@btcpower.com>
2022-09-12 23:12:39 +0530

    First commit

---
+++ src/components/main/Home.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import TopNav from './TopNav'
+import SideNav from './SideNav'
+import Footer from './Footer'
+
+export default function Home() {
+  return (
+  <React.Fragment>
+    <TopNav />
+  <div className="container-fluid">
+    <div className="row">
+      <SideNav />
+      <main className="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item"><a href="/">Home</a></li>
+            <li className="breadcrumb-item active" aria-current="page">Dashboard</li>
+          </ol>
+        </nav>
+        <h1 className="h2">Dashboard</h1>
+        <p>This is the homepage of a simple admin interface which is part of a tutorial written on Themesberg</p>
+        <div className="row my-4">
+          <div className="col-12 col-md-6 col-lg-3 mb-4 mb-lg-0">
+            <div className="card">
+              <h5 className="card-header">Customers</h5>
+              <div className="card-body">
+                <h5 className="card-title">345k</h5>
+                <p className="card-text">Feb 1 - Apr 1, United States</p>
+                <p className="card-text text-success">18.2% increase since last month</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 mb-4 mb-lg-0 col-lg-3">
+            <div className="card">
+              <h5 className="card-header">Revenue</h5>
+              <div className="card-body">
+                <h5 className="card-title">$2.4k</h5>
+                <p className="card-text">Feb 1 - Apr 1, United States</p>
+                <p className="card-text text-success">4.6% increase since last month</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 mb-4 mb-lg-0 col-lg-3">
+            <div className="card">
+              <h5 className="card-header">Purchases</h5>
+              <div className="card-body">
+                <h5 className="card-title">43</h5>
+                <p className="card-text">Feb 1 - Apr 1, United States</p>
+                <p className="card-text text-danger">2.6% decrease since last month</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 mb-4 mb-lg-0 col-lg-3">
+            <div className="card">
+              <h5 className="card-header">Traffic</h5>
+              <div className="card-body">
+                <h5 className="card-title">64k</h5>
+                <p className="card-text">Feb 1 - Apr 1, United States</p>
+                <p className="card-text text-success">2.5% increase since last month</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Footer />
+
+      </main>
+    </div>
+  </div>
+    </React.Fragment>
+  )
+}
---
+++ src/components/main/TopNav.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function TopNav() {
+  return (
+    <React.Fragment>
+    <nav className="navbar navbar-light bg-light p-3">
+    <div className="d-flex col-12 col-md-3 col-lg-2 mb-2 mb-lg-0 flex-wrap flex-md-nowrap justify-content-between">
+      <a className="navbar-brand" href="/">
+        Suncor
+      </a>
+      <button className="navbar-toggler d-md-none collapsed mb-3" type="button" data-toggle="collapse" data-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+    </div>
+    <div className="col-12 col-md-4 col-lg-2">
+    </div>
+    <div className="col-12 col-md-5 col-lg-8 d-flex align-items-center justify-content-md-end mt-3 mt-md-0">
+    <div className="mr-3 mt-1">
+      <div className="image">
+        <img className="rounded-circle" src="https://gurayyarar.github.io/AdminBSBMaterialDesign/images/user.png" width={48} height={48} alt="User"  />
+      </div>
+      </div>
+      <div className="dropdown">
+        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
+          Hello, John Doe
+        </button>
+        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <li><a className="dropdown-item" href="/">Settings</a></li>
+          <li><a className="dropdown-item" href="/">Messages</a></li>
+          <li><a className="dropdown-item" href="/">Sign out</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+    </React.Fragment>
+  )
+}
